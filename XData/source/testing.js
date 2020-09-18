eab04931b92f6c6991652e5d0977d08c50062f45
Jeff Hykin <jeff.hykin@gmail.com>
2020-09-18 07:21:42 -0500

    add parseValue, remove jsonc

--- XData/source/testing.js
+++ XData/source/testing.js
@@ -1,16 +1,14 @@
 const path = require('path')
-const jsonc = require('jsonc')
 const fs = require("fs")
 const {get} = require("good-js")
 
 let regenerateTestTemplate = (filepath) => {
     let testObject = require(filepath)
-    jsonc.readSync(filepath)
     let importPath = path.join(path.dirname(filepath), testObject.from)
     let requiredFile = require(importPath)
     let resultingTool = get({keyList: testObject.import, from: requiredFile})
     
-    console.log(`testing:`,resultingTool)
+    console.debug(`testing:`,resultingTool)
     
     for (let each of testObject.expectedIo) {
         try {
@@ -21,7 +19,7 @@ let regenerateTestTemplate = (filepath) => {
     }
     
     // overwrite the test file
-    jsonc.writeSync(filepath, testObject, {space: 4})
+    fs.writeFileSync(filepath, JSON.stringify(testObject, 0, 4))
 }
 
 
