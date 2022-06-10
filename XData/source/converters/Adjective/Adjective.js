9ae421733939d3515bfda2b9154508a9117dfe6a
Jeff Hykin <jeff.hykin@gmail.com>
2022-06-09 19:55:49 -0500

    add custom adjectives

---
+++ XData/source/converters/Adjective/Adjective.js
@@ -0,0 +1,52 @@
+import { Token, Node, createConverter, converters, convertComponent } from "../../structure.js"
+import * as utils from "../../utils.js"
+import * as tools from "../../xdataTools.js"
+
+export const Adjective = createConverter({
+    decoderName: "Adjective",
+    xdataStringToNode({ string, context }) {
+        var remaining = string
+        // doesnt care about the context.name: "topLevel", "key", "referenceEvaulation", "restOfLineValue", "spanningLinesValue", "indentedValue"
+        let components = {
+            preWhitespace: null, // token
+            content: null,
+            postWhitespace: null, // token
+            comma: null,
+        }
+
+        // 
+        // preWhitespace
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: / */, from: remaining }); if (extraction == null) { return null }
+        components.preWhitespace = new Token({string:extraction})
+
+        // 
+        // content
+        // 
+        while ()
+        var { remaining, extraction } = utils.extractFirst({ pattern: /\(/, from: remaining }); if (extraction == null) { return null }
+        components.content = new Token({string:extraction})
+        
+        // 
+        // postWhitespace
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: / */, from: remaining }); if (extraction == null) { return null }
+        components.postWhitespace = new Token({string:extraction})
+        
+        // 
+        // comma (optional)
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: /,/, from: remaining })
+        components.comma = new Token({string:extraction})
+        
+        // 
+        // return
+        // 
+        return new Node({
+            decodeAs: "Adjective",
+            originalContext: context,
+            childComponents: components,
+            formattingInfo: {},  
+        })
+    },
+})
---
+++ XData/source/converters/CustomAdjectives/CustomAdjectives.js
@@ -0,0 +1,79 @@
+import { Token, Node, createConverter, converters, convertComponent } from "../../structure.js"
+import * as utils from "../../utils.js"
+import * as tools from "../../xdataTools.js"
+
+export const CustomAdjectives = createConverter({
+    decoderName: "CustomAdjectives",
+    xdataStringToNode({ string, context }) {
+        const originalContext = context
+        var remaining = string
+        let components = {
+            preWhitespace: null, // token
+            openParenthese: null,
+            content: [], // array
+            trailingWhitespace: null,
+            closeParenthese: null,
+            postWhitespace: null, // token
+        }
+
+        // 
+        // preWhitespace
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: / */, from: remaining }); if (extraction == null) { return null }
+        context = context.advancedBy(components.preWhitespace = new Token({string:extraction}))
+        
+        // 
+        // (
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: /\(/, from: remaining }); if (extraction == null) { return null }
+        context = context.advancedBy(components.openParenthese = new Token({string:extraction}))
+
+        // 
+        // content
+        // 
+        while (true) {
+            // try to get an adjective
+            extraction = converters.Adjective.xdataStringToNode({
+                string: remaining,
+                context,
+            })
+            if (extraction == null) {
+                break
+            }
+            context = context.advancedBy(extraction)
+            components.content.push(extraction)
+            // end of list (or should be)
+            if (extraction.childComponents.comma == null) {
+                break
+            }
+        }
+        
+        // 
+        // trailingWhitespace
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: / */, from: remaining }); if (extraction == null) { return null }
+        context = context.advancedBy(components.trailingWhitespace = new Token({string:extraction}))
+
+        // 
+        // )
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: /\)/, from: remaining }); if (extraction == null) { return null }
+        context = context.advancedBy(components.closeParenthese = new Token({string:extraction}))
+        
+        // 
+        // postWhitespace
+        // 
+        var { remaining, extraction } = utils.extractFirst({ pattern: / */, from: remaining }); if (extraction == null) { return null }
+        context = context.advancedBy(components.postWhitespace = new Token({string:extraction}))
+        
+        // 
+        // return
+        // 
+        return new Node({
+            decodeAs: "CustomAdjectives",
+            originalContext,
+            childComponents: components,
+            formattingInfo: {},  
+        })
+    },
+})
